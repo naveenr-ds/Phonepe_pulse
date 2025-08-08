f139666b6157f9e0defbd546c3bdeaab11ee2724
Naveen Kumar R <naveenramu2003@gmail.com>
2025-08-08 14:56:39 +0530

    Update streamlit.py
    Dashboard Creation File Added

--- streamlit.py
+++ streamlit.py
@@ -907,6 +907,7 @@ elif selected == "📄 Docs":
 
     **📌 Developed by:** Naveen Kumar R  
     **📧 Email:** naveenramu2003@gmail.com
+    **   Linked In : **https://www.linkedin.com/in/naveenkumar-ds
 
     ---
 
